c4b02c11fb853d578ebb3f0fe2f39f7e7e7e20f7
Andrey Maksimov <fantoff2010@yandex.ru>
2021-02-12 17:01:49 +0300

    Changed readme and made some code formatting

--- api_site/argparser.py
+++ api_site/argparser.py
@@ -5,5 +5,5 @@ api_site_configuration = argparse.ArgumentParser(add_help=True)
 api_site_configuration.add_argument(
     '--debug', '-d',
     action='store_true',
-    help='This argument switch on debug mod',
+    help='This argument switch on debug mode',
 )
