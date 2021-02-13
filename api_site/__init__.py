89a338916727c728cee816c1194e4f542ef20868
Andrey Maksimov <fantoff2010@yandex.ru>
2021-02-13 14:43:37 +0300

    removed pdb

--- api_site/__init__.py
+++ api_site/__init__.py
@@ -4,7 +4,7 @@ from api_site.argparser import api_site_configuration
 
 app = Flask(__name__)
 args = api_site_configuration.parse_known_args()[0]
-# import pdb; pdb.set_trace()
+
 if args.debug:
     app.config.from_object('SETTINGS.Config')
 else:
