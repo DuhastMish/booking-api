858d39be7511a8b0f42dd49495d929cbd44374e6
Andrey Maksimov <fantoff2010@yandex.ru>
2021-05-16 18:04:15 +0300

    added pseudo generate apartaments for hotels

--- booking_api/api_handlers.py
+++ booking_api/api_handlers.py
@@ -82,31 +82,35 @@ def post_hotel():
     return 'Hotels received with args'
 
 
-@app.route('/api/hotels_put', methods=['PUT'])
-def put_hotel():
-    """Сhange room and booking dates."""
-    return 'Booking for user id'
-
-
 @app.route('/api/hotels_delete', methods=['DELETE'])
 def delete_booking():
     """Delete booking."""
     return 'Booking deleted with args'
 
 
-@app.route('/api/insert_hotel')
-def insert_hotel():
+@app.route('/api/insert_pseudo')
+def insert_pseudo():
     """Insert pseudo hotels."""
     hotels_insert.delay()
     return "Pseudo hotel added"
 
 
-@celery.task(name='celery.insert_hotel')
+@celery.task(name='celery.insert_pseudo')
 def hotels_insert():
     """Insert pseudo hotels."""
-    hotel_names = ['First World Hotel & Plaza', 'CityCenter', 'MGM Grand', 'Ambassador City Jomtien']
+    hotel_names = ['First World Hotel & Plaza', 'CityCenter', 'MGM Grand', 'Ambassador City Jomtien',
+                   'Hotel Paris', 'Four Seasons Hotel Firenze', 'Laurus Al Duomo',
+                   'NH Collection Firenze Porta Rossa', 'Grand Hotel Minerva']
+    apartaments_names = ['Улучшенный номер с кроватью размера «king-size»',
+                         'Номер Делюкс с кроватью размера «king-size»',
+                         'Люкс "Салон"', 'Представительский люкс Four Seasons с кроватью размера "king-size"',
+                         'Представительский люкс с кроватью размера «king-size» - Вид на собор',
+                         'Люкс с кроватью размера «king-size» и видом на бассейн и сад',
+                         'Королевский люкс с кроватью размера «king-size»',
+                         'Номер "Четыре сезона" с кроватью размера "king-size"']
+
     cities = ['Jakarta', 'Manila', 'Istanbul', 'Lagos', 'Paris', 'London', 'Toronto']
-    for i in range(100):
+    for _i in range(randint(1, 10)):
         hotel_obj = Hotel(
             name=choice(hotel_names),
             stars=randint(1, 5),
@@ -114,5 +118,14 @@ def hotels_insert():
             price=randint(1000, 15000),
             city=choice(cities),
         )
+
+        for _index in range(randint(1, 10)):
+            apartament_obj = Apartament(
+                name=choice(apartaments_names),
+                price=randint(1000, 15000),
+                beds=randint(1, 4),
+            )
+            hotel_obj.apartament.append(apartament_obj)
+
         session.add(hotel_obj)
     session.commit()
