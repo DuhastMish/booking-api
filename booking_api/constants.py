915cb2e0fd95f247571c0e117a7d81faa527675a
Andrey Maksimov <fantoff2010@yandex.ru>
2021-05-17 21:43:03 +0300

    Heroku string connection

--- booking_api/constants.py
+++ booking_api/constants.py
@@ -1,7 +1,7 @@
 from booking_api.app import app
 
 # postgresql://username:password@localhost:5432/dbname
-SQL_STRING = 'postgres://wwdhglocyynans:9e7337da111a9eaf88d24ea1524efa6e0ba50a4c383a33809cdffcec98a5fd27@'\
+SQL_STRING = 'postgresql://wwdhglocyynans:9e7337da111a9eaf88d24ea1524efa6e0ba50a4c383a33809cdffcec98a5fd27@'\
              'ec2-54-228-139-34.eu-west-1.compute.amazonaws.com:5432/d4iq8notnsdpiv'
 
 CACHE_DEFAULT_TIMEOUT = app.config.get('CACHE_DEFAULT_TIMEOUT')
